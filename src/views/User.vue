ac51ad12ff72db6d5dc1f1b98f46255207b73dd0
xfan0516 <306450948@qq.com>
2020-06-09 23:19:27 +0800

    关于

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,7 +2,7 @@
   <div class="user">
     <div><img src="./../assets/images/userpic.jpg" alt=""></div>
     <Header title="我的"></Header>
-    <x-cell @click="$router.push('xfan')">
+    <x-cell @click="handleClick">
       <span slot="cell-left">xfan组件</span>
       <span slot="cell-right"><x-icon type="ios-arrow-forward" size="big"/></span>
     </x-cell>
@@ -14,6 +14,13 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your user '
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$Message.info('维护中...')
+      return false
+      // this.$router.push('xfan')
     }
   }
 }
